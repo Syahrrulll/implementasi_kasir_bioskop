c33e28d9748d7efbcd253241149070a448c9439e
Syahrrulll <Syahrulmubarak3225@gmail.com>
2024-10-06 23:57:55 +0800

    Update lagi

--- Projek_Bioskop/beranda.php
+++ Projek_Bioskop/beranda.php
@@ -19,7 +19,26 @@
     <div class='sign_in'></div>
     <a href='signin.php' class='btn_signin'>Sign In</a>
 
-    <div clas='component1'></div>
+    <div class='component1'>
+
+        <div class="slides">
+            <span id="slide-1"></span>
+            <span id="slide-2"></span>
+            <span id="slide-3"></span>
+            <div class='image'>
+                <img src='image/slide_1.png'>
+                <img src='image/slide_2.png'>
+                <img src='image/slide_3.png'>
+            </div>
+        </div>
+
+
+        <div class='navigates'>
+            <a href="#slide-1">1</a>
+            <a href="#slide-2">2</a>
+            <a href="#slide-3">3</a>
+        </div>
+    </div>
 
 </body>
 
