c5b58f8ed027122e7c3d02c8edb1f1dfdda59f50
Syahrrulll <Syahrulmubarak3225@gmail.com>
2024-10-13 14:56:12 +0800

    Perbaiki Database

--- Projek_Bioskop.com/service/database.php
+++ Projek_Bioskop.com/service/database.php
@@ -5,9 +5,21 @@ $username = 'root';
 $password = '';
 $database_name = 'bioskop';
 
+$db =  mysqli_connect($hostname, $username, $password);
+
+$check_db = $db->query("SHOW DATABASES LIKE '$database_name'");
+
+if ($check_db->num_rows == 0) {
+
+    if ($db->query("CREATE DATABASE $database_name") === TRUE) {
+        echo "<p class='success'>Database $database_name berhasil dibuat.</p>";
+    } else {
+        echo "<p class= 'error'>Error membuat database: " . $conn->error . "</p>";
+    }
+}
+
 $db = mysqli_connect($hostname, $username, $password, $database_name);
 
-if($db->connect_error){
+if ($db->connect_error) {
     die("error!");
 }
-?>
