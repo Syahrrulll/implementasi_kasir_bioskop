fbc52b092149b5eed4a6c62663725d7e1eaf0fea
Syahrrulll <Syahrulmubarak3225@gmail.com>
2024-10-14 14:23:51 +0800

    Update Interface Beranda

--- Projek_Bioskop.com/index.php
+++ Projek_Bioskop.com/index.php
@@ -34,51 +34,57 @@ $_SESSION['admin'] = false;
 
 
 
-        <div class="kepala">
-
-            <div class="bg_header" style="
+        <div class="kepala"></div>
+        <div class="bg_header" style="
              background-image: url('image/header.png');
              background-size: cover;
              z-index: 0;"></div>
 
 
-            <div class="welcome">
-                SELAMAT DATANG DI WEB KEBIN
-            </div>
-            <div class="description">Nikmati Film Favoritmu, Pesan Tiket dengan Mudah dan Cepat!</div>
-            <div class="batas_header"></div>
+        <div class="welcome">
+            SELAMAT DATANG DI WEB KEBIN
         </div>
-
-        <?php
-        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-
-            include 'layout/logout.html';
-        } else {
-            include 'layout/guest.html';
-        }
-
-        if (isset($_SESSION['alert']) && $_SESSION['alert'] == true) {
-            echo '<script>alert("Berhasil Masuk")</script>';
-            $_SESSION['alert'] = false;
-        } else if (isset($_SESSION['alertadmin']) && $_SESSION['alertadmin'] == true) {
-            echo '<script>alert("Anda Bukan Admin")</script>';
-            $_SESSION['alertadmin'] = false;
-        }
-        ?>
-        <div class="caption">Dapatkan tiket film favoritmu kapan saja, di mana saja, bebas antri dan dijamin aman!</div>
-
-        <div class="component1">
-            <div class="image">
-                <img src="image/slide_1.png" alt="Slide 1">
-                <img src="image/slide_2.png" alt="Slide 2">
-                <img src="image/slide_3.png" alt="Slide 3">
+        <div class="description">Nikmati Film Favoritmu, Pesan Tiket dengan Mudah dan Cepat!</div>
+        <div class="batas_header">
             </div>
         </div>
+        
+        <div class="navibar">
+            <a href="./">Beranda</a>
+            <a href="Jadwal.php">Jadwal</a>
+            
+        </div>
+    <?php
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+
+        include 'layout/logout.html';
+    } else {
+        include 'layout/guest.html';
+    }
+
+    if (isset($_SESSION['alert']) && $_SESSION['alert'] == true) {
+        echo '<script>alert("Berhasil Masuk")</script>';
+        $_SESSION['alert'] = false;
+    } else if (isset($_SESSION['alertadmin']) && $_SESSION['alertadmin'] == true) {
+        echo '<script>alert("Anda Bukan Admin")</script>';
+        $_SESSION['alertadmin'] = false;
+    }
+    ?>
+
+    <div class="caption">Dapatkan tiket film favoritmu kapan saja, di mana saja, bebas antri dan dijamin aman!</div>
+
+    <div class="component1">
+        <div class="image">
+            <img src="image/slide_1.png" alt="Slide 1">
+            <img src="image/slide_2.png" alt="Slide 2">
+            <img src="image/slide_3.png" alt="Slide 3">
+        </div>
+    </div>
 
 
 
 
-        <img src="image/KEBINpng.png" class="watermark-kebin" alt="Watermark Kebin">
+    <img src="image/KEBINpng.png" class="watermark-kebin" alt="Watermark Kebin">
 
 
     </div>
@@ -94,12 +100,11 @@ $_SESSION['admin'] = false;
             <div class="Item"><img src="image/frame7.jpeg"></div>
         </div>
     </div>
-    <div class="navbar">
-        <a href="../Projek_Bioskop.com">
-            <img src="image/Home.png" class="home_logo">
-        </a>
-    </div>
 
+
+    <div class="footer">
+        @Kelompok 14
+    </div>
 </body>
 
 </html>
